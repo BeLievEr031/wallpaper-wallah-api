7bfdfcfeb6fff18d113e0ea61d07a78293b3deb8
BeLieVer031 <sandyrajak031@gmail.com>
2025-08-20 16:07:17 +0530

    feat: add get api with caching.

--- src/controller/category.controller.ts
+++ src/controller/category.controller.ts
@@ -27,8 +27,48 @@ export const create = async (c: Context<{ Bindings: Bindings; }>) => {
         await c.env.WALLPAPER_WALLAH_KV.put(`category:${obj.id}`, JSON.stringify(obj))
 
         return c.json({
-            message: "Category created successfully!!!"
+            message: "Category created successfully!!!",
+            obj
         })
+
+    } catch (error) {
+        return c.json({
+            error: error instanceof Error ? error.message : String(error)
+        }, 400)
+    }
+}
+
+
+export const listCategories = async (c: Context<{ Bindings: Bindings }>) => {
+    try {
+        const limit = Number(c.req.query("limit") || 10)
+        const cursor = c.req.query("cursor")
+
+        const cache = caches.default
+        const cacheKey = new Request(c.req.url)
+
+        let response = await cache.match(cacheKey)
+        if (response) {
+            return response;
+        }
+
+
+        const list = await c.env.WALLPAPER_WALLAH_KV.list({
+            prefix: "category:",
+            limit,
+            cursor: cursor || undefined
+        })
+
+        const categories = await Promise.all(
+            list.keys.map((k) => c.env.WALLPAPER_WALLAH_KV.get(k.name, "json"))
+        )
+
+        response = Response.json({ categories })
+
+        response.headers.append("Cache-Control", "max-age=84000")
+        await cache.put(cacheKey, response.clone())
+
+        return response
 
     } catch (error) {
         return c.json({
--- src/routes/category.route.ts
+++ src/routes/category.route.ts
@@ -1,12 +1,13 @@
 import { Hono } from "hono";
 import { Bindings } from "../types/binding";
-import { create } from "../controller/category.controller";
+import { create, listCategories } from "../controller/category.controller";
 
 
 
 const categoryRoute = new Hono<{ Bindings: Bindings }>();
 
 categoryRoute.post("/", create)
+categoryRoute.get("/", listCategories)
 
 
 export default categoryRoute;
